00988b9e06956683ca5f4ef1987e75996817b827
Usmanjini <48280787+usmanjini@users.noreply.github.com>
2023-06-19 11:54:37 +0500

    add asp project

---
+++ rrstek_asp/Ewms/Ewms/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Ewms.Models;
+
+namespace Ewms.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+     
+    public IActionResult Alljobs()
+    {
+        return View();
+    }
+
+    public IActionResult Activejobs()
+    {
+        return View();
+    }
+
+    public IActionResult Machine()
+    {
+        return View();
+    }
+
+    public IActionResult Quantity()
+    {
+        return View();
+    }
+
+    public IActionResult Requests()
+    {
+        return View();
+    }
+
+    public IActionResult Myjobs()
+    {
+        return View();
+    }
+
+    public IActionResult Startjob()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
